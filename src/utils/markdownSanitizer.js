4eb80a116a9f42a74017790d355342a5ded9a39e
Bharath Kumar <bharath.reddy@webileapps.com>
2025-02-05 16:44:45 +0530

    allowing images in concat santizier

--- src/utils/markdownSanitizer.js
+++ src/utils/markdownSanitizer.js
@@ -6,7 +6,7 @@ const turndownService = new require("turndown")();
 function sanitizeMakrdown(markdown) {
   const html = marked.parse(markdown);
   const sanitizeHtmltext = sanitizeHtml(html, {
-    allowedTags: sanitizeHtml.defaults.allowedTags
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
   });
   return turndownService.turndown(sanitizeHtmltext);
 }
